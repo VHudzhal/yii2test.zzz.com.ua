51ff4683a08fc1a8bb24ef21f9f1f351fcc34344
VHudzhal <vladymyrlem@mail.ru>
2017-10-30 22:56:53 +0200

    Update second.php

--- views/task/second.php
+++ views/task/second.php
@@ -1,7 +1,7 @@
 <div class = "col-lg-3">
     <ul class="breadcrumb">
         <li class=""><a href="/task/index">Home</a></li>
-        <li class="active">Task2</li>
+        <li class="active">Task №2</li>
     </ul>
     <h1>Task№2</h1>
     <p>2. Нужно написать простенькое приложение для управления древовидным меню (пусть будет дерево категорий)</p>
@@ -9,7 +9,6 @@
     <a href="/web/menu/index" class="btn btn-success">Index</a>
     <a href="/web/menu/create" class="btn-success btn">Create</a>
     <hr>
-
         <div>
             <ul class="catalog">
 		        <?= \app\components\MenuTree::widget(['tpl' => 'menu']); ?>
